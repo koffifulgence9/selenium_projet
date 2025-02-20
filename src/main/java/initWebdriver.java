eee8a2b19fd75a90c782bea8ba14c640493fe367
koffifulgence9 <fulgencekoffi21@gmail.com>
2025-02-20 22:43:34 +0000

    commi

--- src/main/java/initWebdriver.java
+++ src/main/java/initWebdriver.java
@@ -1,13 +1,14 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import io.github.bonigarcia.wdm.webdriver.WebDriverBrowser;
 import org.junit.Test;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class initWebdriver {
-
   @Test
-
   public void t001_initWebdriver() {
-    WebDriverManager.firefoxdriver().setup();
-
+    WebDriverManager.chromedriver().setup(); // sera initialiser en tant que firefox
+    ChromeDriver driver = new ChromeDriver();
+    driver.get("https://amazon.com/");
   }
 }
